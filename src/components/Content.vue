58a00e3c35ab6d5dad22570e7c0ef50b3d444eb6
Tommaso Passarotto <passarotto13@gmail.com>
2021-11-24 17:20:37 +0100

    creazione content

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,6 +1,6 @@
 <template>
   <section>
-      
+      <h1>--> Content goes here</h1>
   </section>
 </template>
 
@@ -12,5 +12,16 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+section {
+  margin-top: 10px;
+  background-color: #1c1c1c;
+  height: 100px;
+  display: flex;
+  align-items: center;
 
+  h1 {
+    color: white;
+    margin-left: 100px;
+  }
+}
 </style>
