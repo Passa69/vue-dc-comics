797aa8fdfd29cac6e5c5dba7468137dd5131d574
Tommaso Passarotto <passarotto13@gmail.com>
2021-11-25 16:19:23 +0100

    aggiunto componente per ciclo di oggetti nel main

--- src/App.vue
+++ src/App.vue
@@ -46,4 +46,9 @@ export default {
   text-align: center;
   color: #2c3e50;
 }
+
+main {
+  margin-top: 20px;
+  background-color: #1c1c1c;
+}
 </style>
---
+++ src/components/Contentcard.vue
@@ -0,0 +1,43 @@
+<template>
+  <section>
+      <div>
+          <img :src="details.thumb" alt="">
+          <p>{{ details.series }}</p>
+      </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Contentcard',
+  props: {
+      details: Object
+  }
+ }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+section {
+
+    div {
+    margin: 25px 20px;
+    height: 150px;
+    width: 150px;
+
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        object-position: top;
+    }
+
+    p {
+        font-size: 15px;
+        color: white;
+    }
+}
+}
+
+
+</style>
